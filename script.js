13024dd82472132a734e52e1eec76a73fbc6f380
KRAN44444 <109621342+KRAN44444@users.noreply.github.com>
2023-01-09 09:57:38 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ let input = document.querySelector("input")
 let clear = document.querySelector(".btn_1")
 let finish = document.querySelector(".btn_2")
 let ul = document.querySelector("ul")
+let tasks = []
 
-
+function load(){
+ let tasks = JSON.parse(localStorage.getItem("tasks"))
+ console.log(tasks)
+  if (tasks.length>0){
+    tasks.forEach(element => {
+      let li =  document.createElement("LI");
+      let img = document.createElement("IMG");
+      let span = document.createElement("SPAN");
+      span.innerHTML = element;
+      img.setAttribute("src","garbage.svg");
+      li.appendChild(img);
+      li.appendChild(span);
+      ul.appendChild(li);
+    });
+  }
+}
 
 input.onkeypress = function(event){
    if (event.keyCode == 13){
@@ -12,11 +28,14 @@ input.onkeypress = function(event){
      let img = document.createElement("IMG")
      let span = document.createElement("SPAN")
      span.innerHTML = input.value
+     tasks.push(input.value)
      img.setAttribute("src","garbage.svg")
      li.appendChild(img)
      li.appendChild(span)
      ul.appendChild(li)
      input.value = ""
+     localStorage.setItem("tasks",JSON.stringify(tasks))
+
    }
 }
 ul.onclick = function(event){
@@ -25,9 +44,14 @@ ul.onclick = function(event){
   if (target.tagName === 'LI'){
     target.classList.toggle('line')
   }
+  if (target.tagName === 'IMG'){
+    target.parentElement.remove()
+  }
 }
 clear.onclick = function(){
   ul.innerHTML = ""
+  tasks = []
+  localStorage.setItem("tasks",JSON.stringify(tasks))
 }
 finish.onclick = function(){
   let list = ul.querySelectorAll("li")
@@ -35,3 +59,4 @@ finish.onclick = function(){
     list[i].classList.add("line")
   }
 }
+load()
